0729a3276cda7d44a5bda99cfe97ae813dab5055
sakthilkv <sakthi13072005@gmail.com>
2025-07-27 10:39:54 +0530

    fix: minor resource fix

---
+++ src/main/java/Game/Mushroom.java
@@ -0,0 +1,39 @@
+package Game;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+
+public class Mushroom extends Melee {
+    public Mushroom(int x, int y, int size) {
+        this.hp = 50;
+        this.maxHp = 50;
+        this.attack = 10;
+        this.size = size;
+        this.x = x;
+        this.y = y;
+        loadSprite("/sprites/mushroom.png");
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Graphics2D g2d, boolean enemy) {
+        if (sprite != null) {
+            if (enemy) {
+                AffineTransform original = g2d.getTransform();
+                g2d.translate(x + size, y);
+                g2d.scale(-1, 1);
+                g2d.drawImage(sprite, 0, 0, size, size, null);
+                g2d.setTransform(original);
+            } else {
+                g2d.drawImage(sprite, x, y, size, size, null);
+            }
+        } else {
+            g2d.setColor(enemy ? Color.RED : Color.GREEN);
+            g2d.fillRect(x, y, size, size);
+        }
+    }
+}
